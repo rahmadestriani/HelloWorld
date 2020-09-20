9910fdf4176e78fe28df4be3d46c40c6056fce48
rahmadestriani <rahma.destriani03@gmail.com>
2020-09-20 19:56:04 +0700

    destri

---
+++ app/src/main/java/com/example/helloworld/activity_RahmaDestriani.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_RahmaDestriani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__rahma_destriani);
+    }
+}
