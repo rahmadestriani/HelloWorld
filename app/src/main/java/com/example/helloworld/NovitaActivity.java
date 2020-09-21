ff4bab33cb00d29247435d19d4d688fd5b85298f
Novita <novitasr0401@gmail.com>
2020-09-21 20:42:10 +0700

    commited

--- app/src/main/java/com/example/helloworld/NovitaActivity.java
+++ app/src/main/java/com/example/helloworld/NovitaActivity.java
@@ -11,6 +11,6 @@ public class NovitaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novita);
-        Log.d("MainActivity", "Hello Novita");
+      
     }
 }
